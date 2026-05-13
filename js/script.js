f744b9998b79eb3fbfb2326ec45ac57f30ce16b9
oktoerka <oktoerka2@gmail.com>
2026-05-13 13:51:05 +0700

    update web

--- js/script.js
+++ js/script.js
@@ -1,14 +1,57 @@
-// Toggle
+// Toggle hamburger
 const navbarMenu = document.querySelector('.bar-nav');
 // hamburger menu diklik
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarMenu.classList.toggle('active');
+    e.preventDefault();
+}
+
+// Togle pencarian
+const searchFrom = document.querySelector('.search-from');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-bttn').onclick = (e) => {
+    searchFrom.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
 }
 
 // klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-bttn');
+
 document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarMenu.contains(e.target)) {
+    if(!hm.contains(e.target) && !navbarMenu.contains(e.target)) {
         navbarMenu.classList.remove('active');
     }
+    
+    if(!sb.contains(e.target) && !searchFrom.contains(e.target)) {
+        searchFrom.classList.remove('active');
+    }
 });
+
+// modal
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.bttn-mata');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+});
+
+
+// klik tombol close
+
+document.querySelector('.modal .tutup-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+};
